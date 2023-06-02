b0a296997bd033ba3ff555c0a52ec89f47cf36a0
Chanan Ippel <cippel@degroot.degrootdruk.nl>
2023-06-02 15:59:53 +0200

    show HTML in mail instead of plain text

--- includes/dc-handlecart.php
+++ includes/dc-handlecart.php
@@ -164,7 +164,8 @@ function digicalculator_add_variable_to_order_items( $item, $cart_item_key, $val
             $mail .= "</table>";
             $mail .= "<p>".json_encode($order)."</p>";
 
-            add_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
+            $content_type = function() { return 'text/html'; };
+            add_filter( 'wp_mail_content_type', $content_type );  
             wp_mail("orders@zogedrukt.nl","Digicalulator product in order ", $mail);
             remove_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
 
