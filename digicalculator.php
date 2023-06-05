e186edcbc65524988b9eb9ec5df1c62a02f35429
Chanan Ippel <cippel@degroot.degrootdruk.nl>
2023-06-05 12:14:10 +0200

    added pages text for brochures

--- digicalculator.php
+++ digicalculator.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @since             1.3.1
+ * @since             1.3.2
  * @package           Digicalculator
  *
  * Plugin Name:       Digicalculator
  * Description:       Special connection with the Digicalculator server.
- * Version:           1.3.2
+ * Version:           1.3.3
  * Author:            Chanan Ippel
  */
 
