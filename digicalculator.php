5173f6bfd46b1b3de47bafdaf02b94df311916e7
Chanan Ippel <cippel@degroot.degrootdruk.nl>
2024-05-28 11:55:50 +0200

    added icons for quotations in myAccount environment

--- digicalculator.php
+++ digicalculator.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @since             1.6.5
+ * @since             1.7.0
  * @package           Digicalculator
  *
  * Plugin Name:       Digicalculator
  * Description:       Special connection with the Digicalculator server.
- * Version:           1.7.0
+ * Version:           1.7.1
  * Author:            Chanan Ippel
  */
 
